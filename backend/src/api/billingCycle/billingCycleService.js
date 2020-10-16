16100e7695e56f63737e53f11354b44eb86457d9
dudueduardo121 <dudueduardo121@gmail.com>
2020-10-16 13:22:56 -0300

    obtendo sumario de pagamentos

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -6,21 +6,39 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
 BillingCycle.route('get', (req, res, next) => {
-    BillingCycle.find({}, (err, docs) => {
+    BillingCycle.find({}, (err, value) => {
         if(!err) {
-            res.json(docs)
+            res.json(value)
         }else {
             res.status(500).json({errors: [error]})
         }
     })
 })
 
+// rota que conta a qunatidade de registros 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if(error) {
             res.status(500).json({errors: [error]})
         }else {
             res.json({value})
+        }
+    })
+})
+
+// rota Sumario de ciclos de pagamento
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{ 
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+    }, { 
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, { 
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
         }
     })
 })
