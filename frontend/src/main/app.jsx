24530f461e6190346226bea38f2201c126792284
dudueduardo121 <dudueduardo121@gmail.com>
2020-10-16 16:26:57 -0300

    component cabeçalho

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../common/template/dependencies'
+import Header from '../common/template/header'
 
 export default props => {
   return (
     <div className="wrapper">
-        <h1>App</h1>
+       <Header/>
     </div>
   );
 }
