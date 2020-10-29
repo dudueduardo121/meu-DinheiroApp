319bb2a7d385b1ae878f6ff0c164a3e795377c24
dudueduardo121 <dudueduardo121@gmail.com>
2020-10-29 10:26:07 -0300

    Exibir aba alterar ciclo de pagamento

--- frontend/src/biliingCycle/billingCycleActions.js
+++ frontend/src/biliingCycle/billingCycleActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import {toastr} from 'react-redux-toastr'
-import { reset as resetForm} from 'redux-form'
+import { reset as resetForm, initialize} from 'redux-form'
 
 import { showTabs, selectTab } from '../common/tab/tabActions'
 const BASE_URL = 'http://localhost:3003/api'
@@ -30,4 +30,12 @@ export function create(values) {
         })
     }
 
+}
+
+export function showUpdate(billingClycle) {
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingClycle)
+    ]
 }
