7c4556782588a1c65c24e17de3c57ddbeae3ae69
dudueduardo121 <dudueduardo121@gmail.com>
2020-10-29 10:50:42 -0300

    Função botão cancelar

--- frontend/src/biliingCycle/billingCycleActions.js
+++ frontend/src/biliingCycle/billingCycleActions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import {toastr} from 'react-redux-toastr'
 import { reset as resetForm, initialize} from 'redux-form'
-
 import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
+
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -18,12 +20,7 @@ export function create(values) {
         axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação realizada com sucesso.')
-            dispatch([
-                resetForm('billingCycleForm'),
-                getList(),
-                selectTab('tabList'),
-                showTabs('tabList', 'tabCreate')
-            ])
+            dispatch(init())
         })
         .catch(e => {
             e.response.data.errors.forEach(error => toastr.error('Error', error))
@@ -37,5 +34,14 @@ export function showUpdate(billingClycle) {
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingClycle)
+    ]
+}
+
+export function init() {
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
     ]
 }
