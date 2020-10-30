719afb787b8a63c4d2c4ed7d7ebd96aaf648bc1b
dudueduardo121 <dudueduardo121@gmail.com>
2020-10-30 11:30:20 -0300

    componet Credtlist finalizado

--- frontend/src/biliingCycle/billingCycle.jsx
+++ frontend/src/biliingCycle/billingCycle.jsx
@@ -11,15 +11,13 @@ import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create, update, remove } from './billingCycleActions'
+import { init,create, update, remove } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
   //defini tblist como padrão
   componentWillMount() {
-    this.props.selectTab('tabList')
-    this.props.showTabs('tabList', 'tabCreate')
+    this.props.init()
   }
 
   render() {
@@ -55,5 +53,5 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, update, remove}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({init, create, update, remove}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
